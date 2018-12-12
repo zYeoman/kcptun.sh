542882bd74c9659608cb1f48ca7dfac2a6ad66c1
Yongwen Zhuang <zyeoman@163.com>
2018-12-12 20:52:17 +0800

    :pencil: Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
-echo "rm -rf $HOME/.kcptun"
-rm -rf $HOME/.kcptun
+source config.conf
+echo "rm -r $kcpath"
+rm -r "$kcpath"
 echo "rm /usr/local/bin/kcpclient"
 rm /usr/local/bin/kcpclient
-echo "rm /usr/local/bin/kcpserver" 
+echo "rm /usr/local/bin/kcpserver"
 rm /usr/local/bin/kcpserver
 echo "sed -ie '/kcpserver start/d' /etc/rc.local"
 sed -ie '/kcpserver start/d' /etc/rc.local
